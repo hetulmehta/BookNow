1ce010593f8bdc785c801613796ccfb0cde6257a
hetulmehta <hetulmehta08@gmail.com>
2022-11-29 02:36:06 +0530

    changes

--- js/cinemas.js
+++ js/cinemas.js
@@ -1,7 +1,7 @@
 function getCinemas() {
     return [
         {
-            branchName:"City Cinema - Central",
+            branchName:"City Cinema - Mumbai",
             movies:[
                 {
                     id:1,
@@ -10,24 +10,49 @@ function getCinemas() {
                             index:1,
                             datetime:"11 Nov, Wed - 9:00am",
                             house:4
+                        },
+                        {
+                            index:2,
+                            datetime:"11 Nov, Wed - 11:00am",
+                            house:4
+                        },
+                        {
+                            index:3,
+                            datetime:"11 Nov, Wed - 4:00pm",
+                            house:4
+                        },
+                        {
+                            index:4,
+                            datetime:"12 Nov, Thur - 9:00am",
+                            house:4
+                        }
+                    ]
+                },
+                {
+                    id:2,
+                    shows:[
+                        {
+                            index:5,
+                            datetime:"11 Nov, Wed - 8:00am",
+                            house:4
                         }
                     ]
                 }
             ]
         },
         {
-            branchName:"City Cinema - Mong Kok",
+            branchName:"City Cinema - Pune",
             movies:[
                 {
-                    id:2,
+                    id:3,
                     shows:[
                         {
-                            index:2,
+                            index:6,
                             datetime:"11 Nov, Wed - 9:00am",
                             house:4
                         },
                         {
-                            index:3,
+                            index:7,
                             datetime:"12 Nov, Thu - 11:00am",
                             house:2
                         }
@@ -36,28 +61,19 @@ function getCinemas() {
             ]
         },
         {
-            branchName:"City Cinema - Festival Walk",
+            branchName:"City Cinema - Bangalore",
             movies:[
                 {   id:1,
                     shows:[
                     {   
-                        index:4,
+                        index:8,
                         datetime:"8 Nov, Sun - 9:00am",
                         house:3            
                     },
                     {   
-                        index:5,
+                        index:9,
                         datetime:"10 Nov, Tue - 11:00am",
                         house:5            
-                    }
-                    ]
-                },
-                {   id:3,
-                    shows:[
-                    {   
-                        index:6,
-                        datetime:"9 Nov, Mon - 2:00pm",
-                        house:1            
                     }
                     ]
                 }
--- js/movies.js
+++ js/movies.js
@@ -3,62 +3,56 @@ function getMovies() {
         {
             id:1,
             type:"now",
-            thumbnail:"image/thumbnails/casablanca.jpg",
-            src:"http://courses.cs.cityu.edu.hk/cs2204/video/casablanca-s.mp4",
-            name:"Cassblanca",
-            cast:"Humphrey Bogart, Ingrid Bergmen",
-            director:"Michael Curtiz",
-            duration: 130
+            thumbnail:"image/thumbnails/bp.jpg",
+            name:"Black Panther:Wakanda Forever",
+            cast:"Chadwick Boseman, Michael B. Jordan, Lupita Nyong'o",
+            director:"Ryan Coogler",
+            duration: 160
         },
         {
             id:2,
             type:"now",
-            thumbnail:"image/thumbnails/chocolate.jpg",
-            src:"http://courses.cs.cityu.edu.hk/cs2204/video/chocolate.mp4",
-            name:"Chocolate",
-            cast:"Johnny Depp, Ingrid Bergmen",
-            director:"Tim Burton",
-            duration: 115
+            thumbnail:"image/thumbnails/thor.jpg",
+            name:"Thor: Love and Thunder",
+            cast:"Chris Hemsworth, Natalie Portman, Tessa Thompson",
+            director:"Taika Waititi",
+            duration: 120
         },
         {
             id:3,
             type:"now",
-            thumbnail:"image/thumbnails/wonders.jpg",
-            src:"http://courses.cs.cityu.edu.hk/cs2204/video/wonders.mp4",
-            name:"Wonders",
-            cast:"Jacob Tremblay, Noah Jupe",
-            director:"Stephen Chbosky",
-            duration: 130
+            thumbnail:"image/thumbnails/sp.jpg",
+            name:"SpiderMan: No Way Home",
+            cast:"Tom Holland, Zendaya, Jacob Batalon",
+            director:"Jon Watts",
+            duration: 120
         },
         {
             id:4,
             type:"upcoming",
-            thumbnail:"image/thumbnails/bigbuck.jpg",
-            src:"http://courses.cs.cityu.edu.hk/cs2204/video/bigbuck-s.mp4",
-            name:"Bigbuck",
-            cast:"Mark Wahlberg, Freddie Highmore",
-            director:"Sacha Goedegebure",
-            duration: 150
+            thumbnail:"image/thumbnails/ant.jpg",
+            name:"Ant-Man and the Wasp: Quantumania",
+            cast:"Paul Rudd, Evangeline Lilly, Michael Douglas",
+            director:"Peyton Reed",
+            duration: 120
         },
         {
             id:5,
             type:"upcoming",
-            thumbnail:"image/thumbnails/barbecue.jpg",
-            src:"http://courses.cs.cityu.edu.hk/cs2204/video/barbecue.mp4",
-            name:"Barbecue",
-            cast:"Lambert Wilson, Franck Dubosc",
-            director:"Eric Lavaine",
-            duration: 115
+            thumbnail:"image/thumbnails/vol3.png",
+            name:"Guardians of the Galaxy Vol. 3",
+            cast:"Chris Pratt, Zoe Saldana, Dave Bautista",
+            director:"James Gunn",
+            duration: 120
         },
         {
             id:6,
             type:"upcoming",
-            thumbnail:"image/thumbnails/nature.jpg",
-            src:"http://courses.cs.cityu.edu.hk/cs2204/video/nature.mp4",
-            name:"Nature",
-            cast:"Raymond Chiu, Sam Chan",
-            director:"Peter Wong",
-            duration: 75
+            thumbnail:"image/thumbnails/marvels.jpeg",
+            name:"The Marvels",
+            cast:"Brie Larson, Samuel L. Jackson, Gemma Chan",
+            director:"Nia DaCosta",
+            duration: 120
         }
     ]
 }
